ef15567df463e47503efa7358d7d0650ab523218
Tóth Zoltán <thozoo@gmail.com>
2023-10-06 10:21:52 +0200

    Kezdő kép már megvan

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+var t = [
+    {
+        sorrend: [0, 0, 0, 0]
+    },
+    {
+        sorrend: [0, 0, 0, 0]
+    },
+    {
+        sorrend: [0, 0, 0, 0]
+    }
+]
+
+function Berak()
+{
+    for (let i=0; i<t.length; i++)
+    {
+        for (let j=0; j<t[i].sorrend.length; j++)
+        {
+            t[i].sorrend[j] = 
+            Math.floor(Math.random()*7)+1;
+        }
+    }
+    Rajzol();
+}
+
+function Rajzol()
+{
+    for (let i=0; i<t.length; i++)
+    {
+        for (let y of t[i].sorrend)
+        {
+            document.getElementById("s"+(i+1)).innerHTML += "<img class='kiskep' src='assets/images/"+y+".png'>";
+        }
+    }
+}
+
+Berak();    
