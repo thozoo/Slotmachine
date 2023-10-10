283bc46080b7df71779d663bab4bd3ff9ade3c44
Tóth Zoltán <thozoo@gmail.com>
2023-10-10 11:25:34 +0200

    Szar az egész

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,11 +27,27 @@ function Rajzol()
 {
     for (let i=0; i<t.length; i++)
     {
+        var szorzo=0;
         for (let y of t[i].sorrend)
         {
-            document.getElementById("s"+(i+1)).innerHTML += "<img class='kiskep' src='assets/images/"+y+".png'>";
+            document.getElementById("s"+(i+1)).innerHTML += "<img class='kiskep' src='assets/images/"+y+".png' style='top:"+(160*szorzo)+"px'>";
+            szorzo++;
         }
     }
 }
 
-Berak();    
+Berak();
+
+function Forgat()
+{
+    document.getElementById("forgatogomb").disabled = true;
+
+    var k = document.getElementsByClassName("kiskep");
+
+    for (let x of k)
+    {
+        // ITT KELL ÁLLÍTANI A TOPOT!!! DE NEM IS JÓ A TOP
+    }
+
+    //document.getElementById("forgatogomb").disabled = false;
+}
